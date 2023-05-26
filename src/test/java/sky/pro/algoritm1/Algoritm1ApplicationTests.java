6931f278eade61663b7abba29cddfd74fab850dd
Александр <perchatki79@gmail.com>
2023-05-26 18:55:28 +0300

    Initial commit

---
+++ src/test/java/sky/pro/algoritm1/Algoritm1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.algoritm1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Algoritm1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
